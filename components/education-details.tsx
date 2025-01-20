ff9ed5e2ed0bf47c94949277e44880ca5d18621e
DinelyWellehewage <dinelywh78@gmail.com>
2025-01-20 10:12:25 +0100

    format images

--- components/education-details.tsx
+++ components/education-details.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import { useRef } from "react";
 
 import { ListIcon } from "./list-icon";
@@ -10,31 +10,25 @@ type Props = {
   info: string;
 };
 
-export const EducationDetails = ({
-  type,
-  place,
-  time,
-  info,
-}: Props) => {
+export const EducationDetails = ({ type, place, time, info }: Props) => {
   const ref = useRef(null);
 
   return (
-    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]">
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]"
+    >
       <ListIcon reference={ref} />
       <motion.div
         initial={{ y: 50 }}
         whileInView={{ y: 0 }}
-        transition={{ duration: 0.5, type: 'spring' }}
+        transition={{ duration: 0.5, type: "spring" }}
       >
-        <h3 className="font-bold text-2xl sm:text-xl xs:text-lg">
-          {type}
-        </h3>
+        <h3 className="font-bold text-2xl sm:text-xl xs:text-lg">{type}</h3>
         <span className="capitalize font-medium text-dark/75 xs:text-sm">
           {time} | {place}
         </span>
-        <p className="font-medium w-full text-dark">
-          {info}
-        </p>
+        <p className="font-medium w-full text-dark">{info}</p>
       </motion.div>
     </li>
   );
